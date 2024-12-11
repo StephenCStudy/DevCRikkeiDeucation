7b388f9c7f32a901a481eea70d6a3aa7e0d55094
StephenCStudy <n24dtcn003@student.ptithcm.edu.vn>
2024-12-11 20:30:42 +0700

    sesion 5

--- sesion 5.8.cpp
+++ sesion 5.8.cpp
@@ -1,6 +1,29 @@
 #include <stdio.h>
- 
-  int main (){
-  	
-  	return 0;
-  }
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int lcm(int a, int b) {
+    return (a * b) / gcd(a, b);
+}
+
+int main() {
+    int num1, num2;
+
+    printf("nhap vao so nguyen duong (a): ");
+    scanf("%d", &num1);
+    printf("nhap vao so nguyen duong (b): ");
+    scanf("%d", &num2);
+
+    int BCNN = lcm(num1, num2);
+
+    printf("BCNN cua %d va %d la: %d\n", num1, num2, BCNN);
+
+    return 0;
+}
