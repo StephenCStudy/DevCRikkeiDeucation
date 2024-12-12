c7c3650c1fd4206f39b3a38cef3681f84671e10d
StephenCStudy <n24dtcn003@student.ptithcm.edu.vn>
2024-12-12 10:40:31 +0700

    Update sesion 3.8.cpp

--- sesion 3.8.cpp
+++ sesion 3.8.cpp
@@ -6,10 +6,7 @@ int main() {
 
     printf("nhap mot so nguyen to co 4 chu so: ");
     scanf("%d", &num);
-
-    if (num >= 1000 && num <= 9999) {
-    	
-        
+     
             nghichDao = nghichDao * 10 + num % 10;
             num /= 10;
             nghichDao = nghichDao * 10 + num % 10;
@@ -18,12 +15,8 @@ int main() {
             num /= 10;
             nghichDao = nghichDao * 10 + num % 10;
             num /= 10;
-        
-
+    
         printf("so nghich dao la: %d\n", nghichDao);
-    } else {
-        printf("so nhap vao khong phai so co 4 chu so.\n");
-    }
-
+     
     return 0;
 }
