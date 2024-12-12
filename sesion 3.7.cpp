85fbe91c284a12d26f498e8cae1f5efeff5129ea
StephenCStudy <n24dtcn003@student.ptithcm.edu.vn>
2024-12-12 10:41:08 +0700

    Update sesion 3.7.cpp

--- sesion 3.7.cpp
+++ sesion 3.7.cpp
@@ -2,14 +2,9 @@
 
 int main() {
     int so;
-	int tong = 0;
-
+    int tong = 0;
     printf("nhap mot so nguyen to co 4 chu so: ");
-    scanf("%d", &so);
-
-    if (so >= 1000 && so <= 9999) {
-        
-        
+    scanf("%d", &so);              
             tong += so % 10;
             so /= 10;
             tong += so % 10;
@@ -17,14 +12,9 @@ int main() {
             tong += so % 10;
             so /= 10;
             tong += so % 10;
-            so /= 10;
-        
-
-        
+            so /= 10;      
         printf("tong cac chu so la: %d\n", tong);
-    } else {
+   
         printf("so nhap vao khong phai so co 4 chu so.\n");
-    }
-
     return 0;
 }
