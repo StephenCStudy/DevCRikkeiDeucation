c566a4a0d34019676fea82dfdfe05af2bf01839d
StephenCStudy <n24dtcn003@student.ptithcm.edu.vn>
2024-12-17 15:30:56 +0700

    Update sesion 6.9.cpp

--- sesion 6.9.cpp
+++ sesion 6.9.cpp
@@ -1,25 +1,18 @@
 #include <stdio.h>
-#include <math.h>
-
-int is_armstrong(int n) {
-    int number = n;
-    int sum = 0;
-
-    while (n != 0) {
-        int so = n % 10;
-        sum += pow(so, 3);
-        n /= 10;
-    }
-
-    return (sum == number);
-}
 
 int main() {
-    printf("Cac so Armstrong co 3 chu so:\n");
-    for (int i = 100; i < 1000; i++) {
-        if (is_armstrong(i)) {
-            printf("%d\n", i);
+    int num, a, b, c;
+
+    printf("Cac so Armstrong co 3 chu so la:\n");
+    for (num = 100; num < 1000; num++) {
+        a = num / 100;       
+        b = (num / 10) % 10;   
+        c = num % 10;          
+
+        if ((a*a*a) + (b*b*b) + (c*c*c) == num) {
+            printf("%d\n", num);
         }
     }
+
     return 0;
 }
