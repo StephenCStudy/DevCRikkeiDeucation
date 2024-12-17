43ed7806c9915d3cc18527e996fbef531ad1a3a1
StephenCStudy <n24dtcn003@student.ptithcm.edu.vn>
2024-12-17 15:44:49 +0700

    Update sesion 6.1.cpp

--- sesion 6.1.cpp
+++ sesion 6.1.cpp
@@ -1,18 +1,33 @@
 #include <stdio.h>
  
-  int main (){
-  	int so[5];
+int main (){
+  	int so1,so2,so3,so4,so5;
   	int tong = 0;
-  	for (int i=0; i<5; i++){
-  		printf("nhap vao so nguyen duong %d:", (i + 1) );
-  		scanf("%d", &so[i]);
+  	
+  		printf("nhap vao so nguyen duong :"  );
+  		scanf("%d", &so1);
+  		printf("nhap vao so nguyen duong :"  );
+  		scanf("%d", &so2);
+  		printf("nhap vao so nguyen duong :" );
+  		scanf("%d", &so3);
+  		printf("nhap vao so nguyen duong :" );
+  		scanf("%d", &so4);
+  		printf("nhap vao so nguyen duong :" );
+  		scanf("%d", &so5);
+		
   		
-	  }
-	for (int i=0; i<5; i++){
-		if (so[i] % 2 != 0){  
-			tong = tong + so[i];
-	    	}
-      }  
+  		if (so1 % 2 != 0){  
+			tong = tong + so1;
+	    	}  if (so2 % 2 != 0){
+	    		tong = tong + so2;
+			} if (so3 % 2 != 0){
+			    tong = tong + so3;	
+			} if (so4 % 2 != 0){
+				tong = tong + so4;
+			} if (so5 % 2 != 0){
+				tong = tong + so5;
+			}
+	
     printf(" \n \n \n \n Tong cac so le vua nhap la: %d \n",tong);
     
   	return 0;
